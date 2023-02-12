2a1a37c061b3858c046f0c750ebf4cde61834aef
MatchuPitchu <michael.flohr@posteo.de>
2023-02-12 10:24:21 -0400

    feat: add typescript examples

---
+++ 00_typescript-tips-examples/src/enums-and-literals.ts
@@ -0,0 +1,42 @@
+export {};
+
+// Enumeration
+enum LoadingState {
+  BEFORE_LOAD = 'beforeLoad',
+  LOADING = 'loading',
+  LOADED = 'loaded',
+}
+
+const germanLoadingStates = {
+  [LoadingState.BEFORE_LOAD]: 'Noch nicht geladen',
+  [LoadingState.LOADING]: 'Lädt',
+  [LoadingState.LOADED]: 'Geladen',
+};
+
+const isLoading = (state: LoadingState) => state === LoadingState.LOADING;
+
+console.log(isLoading(LoadingState.BEFORE_LOAD));
+
+// Literal Types
+// Numeric Literal Types: type TileSize = 8 | 16 | 32;
+// String Literal Types: type Easing = "ease-in" | "ease-out" | "ease-in-out";
+
+// würfeln
+const rollDice = (dice: 1 | 2 | 3): number => {
+  let total = 0;
+  for (let i = 0; i < dice; i++) {
+    total += Math.floor(Math.random() * 6) + 1;
+  }
+  return total;
+};
+
+console.log(rollDice(4));
+
+// Function Overloading with String Literal in parameter 'name'
+function sendEvent(name: 'addToCart', data: { productId: number }): void;
+function sendEvent(name: 'checkout', data: { cartCount: number }): void;
+function sendEvent(name: string, data: unknown) {
+  console.log(`${name}: ${JSON.stringify(data)}`);
+}
+
+sendEvent('addToCart', { productId: 1234 });
---
+++ 00_typescript-tips-examples/src/mixins.ts
@@ -0,0 +1,78 @@
+export {};
+
+const createLoggerFunction = () => {
+  return (str: string) => console.log(str);
+};
+
+const logger1 = createLoggerFunction();
+logger1('foo');
+
+const createLoggerClass = () => {
+  return class MyLoggerClass {
+    private completeLog: string = '';
+
+    log(str: string) {
+      console.log(str);
+      this.completeLog += `${str} \n`;
+    }
+
+    dumpLog() {
+      return this.completeLog;
+    }
+  };
+};
+
+const MyLogger = createLoggerClass();
+const logger2 = new MyLogger();
+
+logger2.log('Foo');
+console.log(logger2.dumpLog());
+
+const CreateSimpleMemoryDatabase = <T>() => {
+  return class SimpleMemoryDatabase {
+    private db: Record<string, T> = {};
+
+    set(id: string, value: T) {
+      this.db[id] = value;
+    }
+    get(id: string): T {
+      return this.db[id];
+    }
+    getObject(): object {
+      return this.db;
+    }
+  };
+};
+
+type User = {
+  name: string;
+  city: string;
+};
+
+const UserDatabase = CreateSimpleMemoryDatabase<User>();
+
+const userDb1 = new UserDatabase();
+// userDb1.set('1', 123); // Type Error
+userDb1.set('1', { name: 'Matchu', city: 'Berlin' });
+
+// Mixin
+type Constructor<T> = new (...args: any[]) => T; // constructor functions can be identified with new keyword
+
+// Notice: The object type represents all non-primitive values while the Object type describes the functionality of all objects.
+// For example, the Object type has the toString() and valueOf() methods that can be accessible by any object.
+type MandatoryMethods = { getObject(): object };
+
+const Dumpable = <T extends Constructor<MandatoryMethods>>(Base: T) => {
+  return class Dumpable extends Base {
+    dump() {
+      console.log(this.getObject());
+    }
+  };
+};
+
+const DumpableStringDatabase = Dumpable(UserDatabase);
+
+const userDb2 = new DumpableStringDatabase();
+// userDb2.set('1', 123); // Type Error
+userDb2.set('1', { name: 'Matchu', city: 'Berlin' });
+userDb2.dump(); // { 1: { name: 'Matchu', city: 'Berlin' }
