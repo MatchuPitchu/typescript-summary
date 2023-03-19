5776363c5ac1ed5cd9366f352d6f9c63e287eaea
MatchuPitchu <michael.flohr@posteo.de>
2023-03-19 15:29:20 +0100

    feat: update satisfies operator

--- 00_typescript-tips-examples/src/satisfies.ts
+++ 00_typescript-tips-examples/src/satisfies.ts
@@ -8,18 +8,17 @@ const color1: ColorObj = {
   hex: '#ffffff',
 };
 
-const rgb1 = color1.rgb; // RGB | `#${string}` | undefined
-const hex1 = color1.hex; // RGB | `#${string}` | undefined
+const { rgb, hex } = color1; // both of type: RGB | `#${string}`
 
-// new satisfies operator with TypeScript 4.9
+// typed with new satisfies operator: TypeScript 4.9
 // let validate that the type of an expression matches some type, without changing the resulting type of that expression
 // -> example below: all properties of color2 obj are compatible with ColorObj
 const color2 = {
   rgb: [255, 0, 0],
   hex: '#ffffff',
-  foo: 'bar', // 'foo' does not exist in type 'ColorObj'
+  foo: 'bar', // TS Error: 'foo' does not exist in type 'ColorObj'
 } satisfies ColorObj;
 
 const rgb2 = color2.rgb; // [number, number, number]
-const rgb3 = color2.rgb[3]; // Tuple type '[number, number, number]' of length '3' has no element at index '3'.
-const hex2 = color2.hex;
+const hex2 = color2.hex; // type "#ffffff"
+const rgb3 = color2.rgb[3]; // TS Error: Tuple type '[number, number, number]' of length '3' has no element at index '3'.
