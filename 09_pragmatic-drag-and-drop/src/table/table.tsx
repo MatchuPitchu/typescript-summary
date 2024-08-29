d1994b20c74cca04d5a1cc6c549b3987c302e343
MatchuPitchu <michael.flohr@posteo.de>
2024-08-29 21:07:26 +0200

    feat: update drag and drop table

--- 09_pragmatic-drag-and-drop/src/table/table.tsx
+++ 09_pragmatic-drag-and-drop/src/table/table.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import { Th } from './th';
 
 import './table.css';
@@ -12,21 +14,36 @@ const tableHeaders = ['Column 1', 'Column 2', 'Column 3'];
  * Achtung: Beispiel funktioniert bisher nur oberflächlich und ist nicht fertig durchdacht und umgesetzt
  */
 export const Table = () => {
+  const tableRef = useRef<HTMLTableElement>(null);
+
+  // Storing the height of the table in a CSS variable.
+  // This is used by our header resizer and drop target.
+  useEffect(() => {
+    if (!tableRef.current) return;
+    const height = tableRef.current.getBoundingClientRect().height;
+    tableRef.current.style.setProperty('--table-height', `${height}px`);
+  }, []); // recompute table height when changes occur that impacts the height
+
   return (
-    <table className="table">
-      <thead>
-        {tableHeaders.map((label, index) => (
-          <Th key={label} index={index} amountOfHeaders={3}>
-            Column 1
-          </Th>
-        ))}
-      </thead>
-      <tbody>
-        <tr>
-          <td>Content 1</td>
-          <td>Content 2</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="table-container">
+      <table ref={tableRef} className="table">
+        <thead>
+          <tr>
+            {tableHeaders.map((label, index) => (
+              <Th key={label} index={index} amountOfHeaders={3}>
+                {label}
+              </Th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="td">Content 1</td>
+            <td className="td">Content 2</td>
+            <td className="td">Content 3</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
--- 09_pragmatic-drag-and-drop/src/table/th.tsx
+++ 09_pragmatic-drag-and-drop/src/table/th.tsx
@@ -4,6 +4,8 @@ import { preventUnhandled } from '@atlaskit/pragmatic-drag-and-drop/prevent-unha
 import { clsx } from 'clsx';
 import { ReactNode, useEffect, useRef, useState } from 'react';
 
+import { scrollJustEnoughIntoView } from '../utils/scroll-just-enough-into-view';
+
 type Edge = 'bottom' | 'left' | 'right' | 'top';
 
 type HeaderState =
@@ -24,10 +26,6 @@ type HeaderState =
   | {
       type: 'resizing';
       initialWidth: number;
-      tableWidth: number;
-      nextHeaderInitialWidth: number;
-      nextHeader: HTMLElement;
-      maxWidth: number;
     };
 
 /**
@@ -60,10 +58,6 @@ const minColumnWidth = 50;
 
 type ColumnType = 'first-of-many' | 'middle-of-many' | 'last-of-many' | 'only-column';
 
-const clamp = ({ value, min, max }: { value: number; min: number; max: number }) => {
-  return Math.max(min, Math.min(value, max));
-};
-
 const getColumnType = ({ index, amountOfHeaders }: { index: number; amountOfHeaders: number }): ColumnType => {
   if (amountOfHeaders === 1) return 'only-column';
   if (index === 0) return 'first-of-many';
@@ -78,96 +72,80 @@ type ThProps = {
 };
 
 export const Th = ({ index, amountOfHeaders, children }: ThProps) => {
-  const [state, setState] = useState<HeaderState>(idleState);
+  const [headerState, setHeaderState] = useState<HeaderState>(idleState);
+  const [width, setWidth] = useState<number | null>(null);
+
   const thRef = useRef<HTMLTableCellElement>(null);
   const resizerRef = useRef<HTMLDivElement>(null);
 
   const columnType: ColumnType = getColumnType({ index, amountOfHeaders });
 
-  const renderResizeHandle: boolean =
-    (state.type === 'idle' || state.type === 'resizing') &&
-    (columnType === 'first-of-many' || columnType === 'middle-of-many');
+  const isResizeHandleEnabled =
+    (headerState.type === 'idle' || headerState.type === 'resizing') &&
+    (columnType === 'first-of-many' || columnType === 'middle-of-many' || columnType === 'last-of-many');
 
   // Setting up the draggable resize handle
   // How resizing works:
   // 1. change the size of the column header being dragged
   // 2. we change the _next_ column header by the opposite amount
   useEffect(() => {
-    if (!renderResizeHandle) return;
-
+    if (!isResizeHandleEnabled) return;
     if (!resizerRef.current) return;
     if (!thRef.current) return;
 
     return draggable({
       element: resizerRef.current,
-      getInitialData() {
+      getInitialData: () => {
         return { type: 'column-resize', index };
       },
-      onGenerateDragPreview({ nativeSetDragImage }) {
+      onGenerateDragPreview: ({ nativeSetDragImage }) => {
         if (!thRef.current) return;
 
+        // Deaktiviere natives Drag-Preview, ist unpassend bei Resizing.
         disableNativeDragPreview({ nativeSetDragImage });
+        /**
+         * Erlaube Drop-Operation, auch wenn kein Drop Target das Drag-Element akzeptiert.
+         * Bei Resizing brauche ich kein Drop Target.
+         * @see https://atlassian.design/components/pragmatic-drag-and-drop/core-package/utilities/#preventunhandled
+         */
         preventUnhandled.start();
 
         const initialWidth = thRef.current.getBoundingClientRect().width;
 
-        const nextHeader = thRef.current.nextElementSibling;
-        if (!(nextHeader instanceof HTMLElement)) return;
-        const nextHeaderInitialWidth = nextHeader.getBoundingClientRect().width;
-
-        const table = thRef.current.closest('table');
-        if (!table) return;
-        const tableWidth = table.getBoundingClientRect().width;
-
-        // We cannot let `nextHeader` get smaller than `minColumnWidth`
-        const maxWidth = initialWidth + nextHeaderInitialWidth - minColumnWidth;
-
-        setState({
+        setHeaderState({
           type: 'resizing',
           initialWidth,
-          tableWidth,
-          nextHeaderInitialWidth,
-          nextHeader,
-          maxWidth,
         });
       },
-      onDrag({ location }) {
+      onDrag: ({ location }) => {
         if (!thRef.current) return;
+        if (!resizerRef.current) return;
+        if (headerState.type !== 'resizing') return;
+        console.log('L', location.current.input.clientX, location.current.dropTargets);
+        // Set the new width of the resized th
+        const deltaX = location.current.input.clientX - location.initial.input.clientX;
+        const newWidth = Math.max(minColumnWidth, headerState.initialWidth + deltaX);
 
-        const diffX = location.current.input.clientX - location.initial.input.clientX;
-
-        if (state.type !== 'resizing') return;
-        const { initialWidth, nextHeaderInitialWidth, nextHeader, maxWidth } = state;
-
-        // Set the width of our header being resized
-        const proposedWidth = clamp({
-          value: initialWidth + diffX,
-          min: minColumnWidth,
-          max: maxWidth,
-        });
-        thRef.current.style.setProperty('--local-resizing-width', `${proposedWidth}px`);
-        console.log('P', diffX);
-        // How much did the width of the header actually change?
-        const actualDiff = proposedWidth - initialWidth;
-
-        // Now we need to make the opposite change to the next header
-        //
-        // Example: we have two columns A and B
-        // If A is resizing to get larger, B needs to get smaller
-        nextHeader.style.setProperty('--local-resizing-width', `${nextHeaderInitialWidth - actualDiff}px`);
+        // mit CSS Variable funktioniert nicht, dass th immer weiter wachsen kann und table container overflowed wird
+        // thRef.current.style.setProperty('--local-resizing-width', `${newWidth}px`);
+        setWidth(newWidth);
       },
-      onDrop() {
+      onDrop: () => {
         preventUnhandled.stop();
-        setState(idleState);
+        setHeaderState(idleState);
       },
     });
-  }, [renderResizeHandle, index, state]);
+  }, [isResizeHandleEnabled, index, headerState]);
 
   return (
-    <th ref={thRef} className={clsx('th', state.type === 'dragging' && 'th--dragging')}>
+    <th
+      ref={thRef}
+      className={clsx('th', headerState.type === 'dragging' && 'th--dragging')}
+      style={width ? { width } : {}}
+    >
       {children}
-      {renderResizeHandle ? (
-        <div ref={resizerRef} className={clsx('resizer', state.type === 'resizing' && 'resizer--resizing')}></div>
+      {isResizeHandleEnabled ? (
+        <div ref={resizerRef} className={clsx('resizer', headerState.type === 'resizing' && 'resizer--resizing')}></div>
       ) : null}
     </th>
   );
